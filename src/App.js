fc7312f752a5f2c495837e611b84b4d632a23a04
Hani Elabed <hani.elabed@gmail.com>
2018-04-22 11:23:06 -0500

    adds connect and mapStateToProps to App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
 import React, { Component } from 'react';
 import TasksPage from './components/TasksPage';
-// import { connect } from 'react-redux'
+import { connect } from 'react-redux'
 
-const mockTasks = [ // #A
-  {
-    id: 1,
-    title: 'Learn Redux',
-    description: 'The store, actions, and reducers, oh my!',
-    status: 'In Progress',
-  },
-  {
-    id: 2,
-    title: 'Peace on Earth',
-    description: 'No big deal.',
-    status: 'In Progress',
-  },
-];
 
 class App extends Component {
   render() {
     return (
       <div className="main-content">
-        <TasksPage tasks={mockTasks} />
+        <TasksPage tasks={this.props.tasks} />
       </div>
     );
   }
 }
-// export default App;
 
-// function mapStateToProps(state) { //#C
-//   return {
-//     tasks: state.tasks  //#D
-//   }
-// }
-// export default connect(mapStateToProps)(App);
-export default App;
+function mapStateToProps(state) { //#C
+  return {
+    tasks: state.tasks  //#D
+  }
+}
+export default connect(mapStateToProps)(App);
+// export default App;
