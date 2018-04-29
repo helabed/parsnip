019d1b741fc0a5ed6a46e7a9fa0796befc609e7c
Hani Elabed <hani.elabed@gmail.com>
2018-04-29 16:18:04 -0500

    replaced many if(...) statements with a switch(...)
    statement for ease of parsing by the human eye

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,29 +2,34 @@ export default function tasks(state = { tasks: [] }, action) {
   console.log('reducers.index.js.tasks: state', state);
   console.log('reducers.index.js.tasks: action', action);
 
-  if (action.type === 'CREATE_TASK') {
-    return { tasks: state.tasks.concat(action.payload) };
-  }
+  switch (action.type) {
 
-  if (action.type === 'EDIT_TASK') {  // #A
-    const { payload } = action;
-    return {
-      tasks: state.tasks.map(task => {  // #B
-        if (task.id === payload.id) {
-          return Object.assign({}, task, payload.params);  // #C
-        }
-        return task;
-      })
+    case 'CREATE_TASK': {
+      return { tasks: state.tasks.concat(action.payload) };
+    }
+
+    case 'EDIT_TASK': {  // #A
+      const { payload } = action;
+      return {
+        tasks: state.tasks.map(task => {  // #B
+          if (task.id === payload.id) {
+            return Object.assign({}, task, payload.params);  // #C
+          }
+          return task;
+        })
+      }
+    }
+
+    case 'FETCH_TASKS_SUCCEEDED': {
+      return { tasks: action.payload.tasks, }
+    }
+
+    case 'CREATE_TASK_SUCCEEDED': {
+      return { tasks: state.tasks.concat(action.payload.task) };
+    }
+
+    default: {
+      return state
     }
   }
-
-  if (action.type === 'FETCH_TASKS_SUCCEEDED') {
-    return { tasks: action.payload.tasks, }
-  }
-
-  if (action.type === 'CREATE_TASK_SUCCEEDED') {
-    return { tasks: state.tasks.concat(action.payload.task) };
-  }
-
-  return state
 }
